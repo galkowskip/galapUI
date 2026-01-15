db1597d6b75153a71576e72c4885b58a609ed04e
galap <galkowski.p97@gmail.com>
2026-01-15 20:03:41 +0100

    feat: enable auto import of components in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: true,
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   modules: [
     '@nuxt/icon',
     '@nuxtjs/tailwindcss'
