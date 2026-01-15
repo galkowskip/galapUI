9e88394d13072a5098c58dfdd497103729765406
galap <galkowski.p97@gmail.com>
2026-01-15 20:18:50 +0100

    feat: configure component auto-import with custom prefix in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  components: true,
+  components: [
+    {
+      path: './components',
+      prefix: 'Ui'
+    }
+  ],
   compatibilityDate: '2025-07-15',
   modules: [
     '@nuxt/icon',
