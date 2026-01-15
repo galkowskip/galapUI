15f10e8f308fc522cd9bceb1158fbc89ab63a113
galap <galkowski.p97@gmail.com>
2026-01-15 19:44:49 +0100

    chore: remove unused eslint and test-utils modules from nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint',
     '@nuxt/icon',
-    '@nuxt/test-utils',
     '@nuxtjs/tailwindcss'
   ]
 })
